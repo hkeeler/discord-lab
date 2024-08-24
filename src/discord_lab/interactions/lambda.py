1f77f7916aeee85b5cd63ab32f63f607ef77b33a
Hans Keeler <hkeeler@users.noreply.github.com>
2024-08-24 07:32:44 +0000

    feat: take 1 adding sig checking

--- src/discord_lab/interactions/lambda.py
+++ src/discord_lab/interactions/lambda.py
@@ -1,15 +1,30 @@
 import json
 
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
+
+DISCORD_PUBLIC_KEY='1276658810685100126'
+
 def handler(event, context):
     print(event)
     body = event.get('body', None)
-    if body:
-        print(body)
-    else:
-        print('no body')
+    headers = event['headers']
+
+    verify_key = VerifyKey(bytes.fromhex(DISCORD_PUBLIC_KEY))
+
+    signature = headers["X-Signature-Ed25519"]
+    timestamp = headers["X-Signature-Timestamp"]
+
+    response_code = 200
+    response_body = json.dumps( {'type': 1} )
+
+    try:
+        verify_key.verify(f'{timestamp}{body}'.encode(), bytes.fromhex(signature))
+    except BadSignatureError:
+        response_code = 401
+        response_body = 'invalid request signature'
+
     return {
-        'statusCode': 200,
-        'body': json.dumps(
-            {'type': 1}
-        )
+        'statusCode': response_code,
+        'body': response_body
     }
