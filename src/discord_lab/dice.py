9465a019431b5adbbd2f854ac6c9d9123c787888
Hans Keeler <hkeeler@users.noreply.github.com>
2024-09-23 07:47:28 +0000

    feat: impl die expr parsing with pyparsing

--- src/discord_lab/dice.py
+++ src/discord_lab/dice.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
-from enum import IntEnum
+from dataclasses import dataclass, field
+from enum import Enum, IntEnum
 import re
 from secrets import randbelow
-from typing import override
+from typing import override, ClassVar
+
+from pyparsing import CaselessLiteral, Combine, Optional, ParseException, ParseResults, Suppress, ZeroOrMore, alphas, nums, Literal, Word
 
 
 class DieParseException(Exception):
@@ -31,28 +33,36 @@ class DieType(IntEnum):
 
 
 @dataclass(frozen=True)
-class DieMultiplier:
+class MultiDieRoll:
+    # FIXME: This should be calculated
+    value: int
+    details: list[DieRoll]
+
+
+@dataclass(frozen=True)
+class MultiDie:
     type: DieType
-    multiplier: int
+    multiplier: int = 1
 
     def __post_init__(self):
         if self.multiplier < 1:
             raise ValueError(f'Die multiplier {self.multiplier} is less than 1')
 
-    def roll(self) -> tuple[int,list[DieRoll]]:
+    def roll(self) -> MultiDieRoll:
         rolls = [self.type.roll() for x in range(self.multiplier)]
+        total = sum(r.value for r in rolls)
 
-        return sum(r.value for r in rolls), rolls
+        return MultiDieRoll(total, rolls)
 
 
     @staticmethod
-    def parse(die_mult: str) -> DieMultiplier:
+    def parse(multi_die_expr: str) -> MultiDie:
 
         # regex for parsing "3D6" style die side and multiplier strings
-        match = re.match(r'^(\d+)?[dD](\d+)$', die_mult.strip())
+        match = re.match(r'^(\d+)?[dD](\d+)$', multi_die_expr.strip())
 
         if not match:
-            raise DieParseException(f'"{die_mult}" is invalid. Should be of the form D20 or 3D6.')
+            raise DieParseException(f'"{multi_die_expr}" is invalid. Should be of the form D20 or 3D6.')
         
         mult = int(match.group(1) or "1")
         parsed_sides = int(match.group(2))
@@ -63,9 +73,200 @@ class DieMultiplier:
             valid_sides = ', '.join([str(x.value) for x in DieType])
             raise DieParseException(f'{parsed_sides}-sided dice not supported. Must be one of: {valid_sides}')
 
-        return DieMultiplier(die_type, mult)
+        return MultiDie(die_type, mult)
 
     @override
     def __str__(self) -> str:
-        return f"{self.multiplier}{self.type.name}"
+        _str = self.type.name
+        if self.multiplier > 1:
+            _str = f"{self.multiplier}{_str}"
 
+        return _str
+
+
+class Operation(Enum):
+    ADD = '+'
+    SUB = '-'
+    NO_OP = None
+
+    def apply(self, val_1: int, val_2: int) -> int:
+        match self:
+            case Operation.ADD:
+                return val_1 + val_2
+            case Operation.SUB:
+                return val_1 - val_2
+            case Operation.NO_OP:
+                return val_1
+            case _:
+                raise ValueError(f"{self.name} operation not supported")
+
+
+@dataclass
+class TermRoll:
+    # FIXME: This should be calculated
+    value: int
+    details: int|MultiDieRoll
+
+
+@dataclass
+class LabeledTerm:
+    term: int|MultiDie
+    label: str|None = None
+
+    def roll(self) -> TermRoll:
+        total:int = 0
+
+        match self.term:
+            case int():
+                return TermRoll(self.term, self.term)
+            case MultiDie():
+                multi_die_roll = self.term.roll()
+                return TermRoll(multi_die_roll.value, multi_die_roll)
+            case _:
+                raise ValueError(f"Term type {type(self.term)} not supported")
+            
+
+    @override
+    def __str__(self) -> str:
+        _str = str(self.term)
+        if self.label:
+            _str += f" ({self.label})"
+
+        return _str
+
+
+@dataclass
+class TermOperation:
+    operation: Operation
+    labeled_term: LabeledTerm
+
+    @override
+    def __str__(self) -> str:
+        _str = str(self.labeled_term)
+        if self.operation.value:
+            _str = f"{self.operation.value} {_str}"
+
+        return _str
+
+
+@dataclass
+class TermOperationResult:
+    value: int
+    term_op: TermOperation
+
+
+@dataclass
+class IntTermOperationResult(TermOperationResult):
+    pass
+
+@dataclass
+class MultiDieTermOperationResult(TermOperationResult):
+    rolls: MultiDieRoll
+
+
+@dataclass
+class DieExprRoll:
+    # FIXME: This should be calculated
+    value: int
+    results: list[TermOperationResult]
+
+
+class DieExprParser:
+
+    def __init__(self):
+        d4 = Literal('4')
+        d6 = Literal('6')
+        d8 = Literal('8')
+        d10 = Literal('10')
+        d12 = Literal('12')
+        d20 = Literal('20')
+        d100 = Literal('100')
+
+        add_op = Literal('+')
+        sub_op = Literal('-')
+        op = (add_op | sub_op).set_parse_action(lambda tokens: Operation(tokens[0]))
+
+        l_paren = Suppress("(")
+        r_paren = Suppress(")")
+        label = Word(alphas + ' _-:', max=20)
+        label_wrap = l_paren + label + r_paren
+
+        integer = Word(nums).set_parse_action(lambda tokens: int(tokens[0]))
+        multiplier = Word(nums)
+        multi_die_sep = CaselessLiteral('D')
+        die_type = d100 | d20 | d12 | d10 | d8 | d6 | d4
+        roll_mult = Combine(Optional(multiplier) + multi_die_sep + die_type).set_parse_action(lambda tokens: MultiDie.parse(tokens[0]))
+
+        term = roll_mult | integer
+        labeled_term = (term + Optional(label_wrap)).set_parse_action(self.tokens_to_term)
+        term_op = (op + labeled_term).set_parse_action(lambda tokens: TermOperation(tokens[0], tokens[1]))
+
+        self.expr = (labeled_term + ZeroOrMore(term_op)).set_parse_action(lambda tokens: DieExpr(tokens[0], tokens[1:]))
+
+    @staticmethod
+    def tokens_to_term(tokens: ParseResults) -> LabeledTerm:
+        term = LabeledTerm(tokens[0])
+        try:
+            term.label = tokens[1]
+        except IndexError:
+            pass
+
+        return term
+
+    def parse(self, expr_str: str) -> DieExpr:
+        return self.expr.parse_string(expr_str)[0]
+
+die_expr_parser = DieExprParser()
+
+
+@dataclass
+class DieExpr:
+    # TODO: Convert this over to just a single list?
+    first_term: LabeledTerm
+    term_ops: list[TermOperation] = field(default_factory=list)
+    parser: ClassVar[DieExprParser] = die_expr_parser
+
+    @classmethod
+    def parse(cls, expr_str: str) -> DieExpr:
+        try:
+            return cls.parser.parse(expr_str)
+        except ParseException as pe:
+            raise DieParseException(f"Invalid die roll expression `{expr_str}`") from pe
+
+    def roll(self) -> DieExprRoll:
+        first_term = self.first_term
+        first_term_term = first_term.term
+        first_term_result: TermOperationResult
+        match first_term_term:
+            case int():
+                first_term_result = IntTermOperationResult(first_term_term, TermOperation(Operation.NO_OP, first_term))
+            case MultiDie():
+                rolls = first_term_term.roll()
+                first_term_result = MultiDieTermOperationResult(rolls.value, TermOperation(Operation.NO_OP, first_term), rolls)
+
+        total = first_term_result.value
+        term_op_results = [first_term_result]
+
+        for term_op in self.term_ops:
+            term_op_term = term_op.labeled_term.term
+            term_op_result: TermOperationResult
+            match term_op_term:
+                case int():
+                    term_op_result = IntTermOperationResult(term_op_term, TermOperation(term_op.operation, term_op.labeled_term))
+                case MultiDie():
+                    rolls = term_op_term.roll()
+                    term_op_result = MultiDieTermOperationResult(rolls.value, TermOperation(term_op.operation, term_op.labeled_term), rolls)
+
+            total = term_op.operation.apply(total, term_op_result.value)
+            term_op_results.append(term_op_result)
+
+        return DieExprRoll(total, term_op_results)
+
+    @override
+    def __str__(self) -> str:
+        _str = str(self.first_term)
+        for term_op in self.term_ops:
+            _str += f" {term_op}"
+
+        return _str
+    
--- tests/discord_lab/test_dice.py
+++ tests/discord_lab/test_dice.py
@@ -1,6 +1,7 @@
+from typing import cast
 import pytest
 
-from discord_lab.dice import DieType
+from discord_lab.dice import DieExpr, DieExprRoll, DieParseException, IntTermOperationResult, MultiDie, DieType, MultiDieTermOperationResult, Operation, LabeledTerm, TermOperation, TermOperationResult
 
 class TestDieType:
 
@@ -52,4 +53,156 @@ class TestDieType:
         assert die_roll.type == DieType.D100
         assert 1 <= die_roll.value <= 100
 
-class Test
+class TestTerm:
+     
+     def test_str_int_no_label(self):
+         term = LabeledTerm(1)
+
+         assert str(term) == '1'
+
+
+     def test_str_int_with_label(self):
+         term = LabeledTerm(2, 'Test int')
+
+         assert str(term) == '2 (Test int)'
+
+
+     def test_str_die_mult_no_label(self):
+         term = LabeledTerm(MultiDie(DieType.D6, 3))
+
+         assert str(term) == '3D6'
+
+
+     def test_str_die_mult_with_label(self):
+         term = LabeledTerm(MultiDie(DieType.D100), 'Test Die Mult')
+
+         assert str(term) == 'D100 (Test Die Mult)'
+
+
+class TestTermOperator:
+
+    def test_str_add_int(self):
+        term_op = TermOperation(Operation.ADD, LabeledTerm(2))
+
+        assert str(term_op) == '+ 2'
+
+    def test_str_add_die_mult(self):
+        term_op = TermOperation(Operation.ADD, LabeledTerm(MultiDie(DieType.D6, 3)))
+
+        assert str(term_op) == '+ 3D6'
+
+    def test_str_sub_int(self):
+        term_op = TermOperation(Operation.SUB, LabeledTerm(5))
+
+        assert str(term_op) == '- 5'
+
+    def test_str_sub_die_mult(self):
+        term_op = TermOperation(Operation.SUB, LabeledTerm(MultiDie(DieType.D4, 6)))
+
+        assert str(term_op) == '- 6D4'
+
+
+class TestDieExpr:
+
+    def test_term_only_int(self):
+        term = LabeledTerm(3)
+        expr = DieExpr(term)
+        expr_str = '3'
+
+        assert str(expr) == expr_str
+        assert DieExpr.parse(expr_str) == expr
+
+        roll = expr.roll()
+        assert roll == DieExprRoll(3, [IntTermOperationResult(3, TermOperation(Operation.NO_OP, LabeledTerm(3)))])
+
+
+    def test_term_only_die_mult(self):
+        term = LabeledTerm(MultiDie(DieType.D20, 2))
+        expr = DieExpr(term)
+        expr_str = '2D20'
+
+        assert str(expr) == expr_str
+        assert DieExpr.parse(expr_str) == expr
+
+        roll = expr.roll()
+        assert len(roll.results) == 1
+        assert 2 <= roll.value <= 40
+        assert roll.results[0].term_op.operation == Operation.NO_OP
+        assert roll.results[0].term_op.labeled_term.term == MultiDie(DieType.D20, 2)
+
+        roll_results = roll.results[0]
+        assert isinstance(roll_results, MultiDieTermOperationResult)
+
+
+    def test_str_single_op_add_int(self):
+        term_1 = LabeledTerm(1)
+        term_2 = LabeledTerm(2)
+        op = Operation.ADD
+
+        expr = DieExpr(term_1, [TermOperation(op, term_2)])
+        expr_str = '1 + 2'
+
+        assert str(expr) == expr_str
+        assert DieExpr.parse(expr_str) == expr
+
+        roll = expr.roll()
+        assert roll == DieExprRoll(3, [
+                IntTermOperationResult(1, TermOperation(Operation.NO_OP, LabeledTerm(1))),
+                IntTermOperationResult(2, TermOperation(Operation.ADD, LabeledTerm(2)))
+            ]
+        )
+
+
+    def test_str_single_op_sub_die_mult(self):
+        term_1 = LabeledTerm(MultiDie(DieType.D20))
+        term_2 = LabeledTerm(2)
+        op = Operation.SUB
+
+        expr = DieExpr(term_1, [TermOperation(op, term_2)])
+        expr_str = 'D20 - 2'
+
+        assert str(expr) == expr_str
+        assert DieExpr.parse(expr_str) == expr
+
+        roll = expr.roll()
+        assert len(roll.results) == 2
+
+
+    def test_str_single_op_sub_die_mult_with_labels(self):
+        term_1 = LabeledTerm(MultiDie(DieType.D20))
+        term_2 = LabeledTerm(2, "STR")
+        op = Operation.SUB
+
+        expr = DieExpr(term_1, [TermOperation(op, term_2)])
+        expr_str = 'D20 - 2 (STR)'
+
+        assert str(expr) == expr_str
+        assert DieExpr.parse(expr_str) == expr
+
+        roll = expr.roll()
+        assert len(roll.results) == 2       
+
+
+    def test_str_single_op_complex(self):
+        term_1 = LabeledTerm(MultiDie(DieType.D20))
+        term_op_1 = TermOperation(Operation.SUB, LabeledTerm(2, "INT"))
+        term_op_2 = TermOperation(Operation.ADD, LabeledTerm(MultiDie(DieType.D4), "Acid"))
+        term_op_3 = TermOperation(Operation.ADD, LabeledTerm(MultiDie(DieType.D6, 2), "Fire"))
+
+        expr = DieExpr(term_1, [term_op_1, term_op_2, term_op_3])
+        expr_str = "D20 - 2 (INT) + D4 (Acid) + 2D6 (Fire)"
+
+        assert str(expr) == expr_str
+        assert DieExpr.parse(expr_str) == expr
+
+        roll = expr.roll()
+        assert len(roll.results) == 4
+
+
+    def test_str_raise_parse_error(self):
+        expr_str = "WTF"
+
+        with pytest.raises(DieParseException) as dpe:
+            DieExpr.parse(expr_str)
+
+        assert str(dpe.value) == f'Invalid die roll expression `{expr_str}`'
