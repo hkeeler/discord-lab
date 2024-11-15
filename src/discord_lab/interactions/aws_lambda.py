cc371a2e83eb009179ce74d6c093bc75b1684aa8
Hans Keeler <hkeeler@users.noreply.github.com>
2024-11-15 02:15:40 +0000

    debug: will _anything_ work in /askroll resp?

--- src/discord_lab/interactions/aws_lambda.py
+++ src/discord_lab/interactions/aws_lambda.py
@@ -220,30 +220,38 @@ def askroll_cmd(req_body: dict) -> tuple[int,dict]:
     if must_beat:
         fields.append({"name": "Must Beat", "value": must_beat, "inline": True})
 
+    #res_data = {
+    #    'type': 4,
+    #    'data': {
+    #        'content': f"```json\n{json.dumps(req_body,indent=4)}\n```",
+    #        'embeds': [
+    #            {
+    #                "description": roll_desc,
+    #                "color": 9807270, # Grey
+    #                "fields": fields,
+    #            }
+    #        ],
+    #        'components': [
+    #            {
+    #                'type': 1,
+    #                'components': [
+    #                    {
+    #                        'type': 2, # Button
+    #                        'label': 'Roll!',
+    #                        'style': 1, # Primary
+    #                        'custom_id': 'roll_click'
+    #                    },
+    #                ]
+    #            }
+    #        ]
+    #    }
+    #}
+
+    content = 'hi'
     res_data = {
-        'type': 4,
+        'type':4,
         'data': {
-            'content': f"```json\n{json.dumps(req_body,indent=4)}\n```",
-            'embeds': [
-                #{
-                #    "description": roll_desc,
-                #    "color": 9807270, # Grey
-                #    "fields": fields,
-                #}
-            ],
-            #'components': [
-            #    {
-            #        'type': 1,
-            #        'components': [
-            #            {
-            #                'type': 2, # Button
-            #                'label': 'Roll!',
-            #                'style': 1, # Primary
-            #                'custom_id': 'roll_click'
-            #            },
-            #        ]
-            #    }
-            #]
+            'content': content
         }
     }
 
